a0ed7704f67758d36586f05098e437ce3506f8d1
Raghu V <dealshv@gmail.com>
2018-09-13 23:17:54 +0530

    Added Doublecheck for MultiThreading

--- DesignPatterns/src/com/singleton/Singleton.java
+++ DesignPatterns/src/com/singleton/Singleton.java
@@ -15,6 +15,21 @@ public class Singleton {
 			return singleton;
 	}
 
+	/**
+	 * Double Checked Locking Way
+	 * @return
+	 */
+	public static Singleton DoubleCheckedGetInstance() {
+		if (singleton == null)
+			synchronized (Singleton.class) {
+				if (singleton==null)
+					 singleton = new Singleton();
+					 return singleton;
+			}
+		else
+			return singleton;
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(Singleton.getInstance());
